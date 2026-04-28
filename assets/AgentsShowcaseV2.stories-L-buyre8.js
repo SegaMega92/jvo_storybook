25a745c5673ece41a287693d69e20717120caac2
Sergey <sergey@Sergeys-MacBook-Pro-2.local>
2026-04-28 19:35:20 +0500

    Updates

---
+++ assets/AgentsShowcaseV2.stories-L-buyre8.js
@@ -0,0 +1,17 @@
+import{A as t}from"./index-BIHyHxwQ.js";import"./preload-helper-D7k33qEn.js";import"./iframe-Bs7tJ9XX.js";import"./index-WUrUDGBu.js";const p={title:"Sections/AgentsShowcaseV2",component:t,parameters:{layout:"fullscreen",docs:{description:{component:"Scroll-driven витрина ИИ-агентов v2. Карточки с текстом слева и иллюстрацией справа, переключение по скроллу или табам."}}},tags:["autodocs"]},e={args:{}},r={args:{},parameters:{viewport:{defaultViewport:"mobile"}}},a={args:{},parameters:{viewport:{defaultViewport:"tablet"}}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {}
+}`,...e.parameters?.docs?.source}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {},
+  parameters: {
+    viewport: {
+      defaultViewport: 'mobile'
+    }
+  }
+}`,...r.parameters?.docs?.source}}};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {},
+  parameters: {
+    viewport: {
+      defaultViewport: 'tablet'
+    }
+  }
+}`,...a.parameters?.docs?.source}}};const i=["Default","Mobile","Tablet"];export{e as Default,r as Mobile,a as Tablet,i as __namedExportsOrder,p as default};
--- assets/index-DyT-c1HZ.js
+++
@@ -1,1 +0,0 @@
-import{g as r}from"./iframe-BWxcWfmx.js";import{r as o}from"./index-BXgKpjmR.js";var t=o();const m=r(t);export{m as R,t as r};
