063009fec097815f9514b3603876840f411712e0
Sergey <sergey@Sergeys-MacBook-Pro-2.local>
2026-04-29 16:57:57 +0500

    Updates

---
+++ assets/IllustrationAdvertising.stories-02JZUTZY.js
@@ -0,0 +1,11 @@
+import{j as e}from"./iframe-BroFZNr1.js";import{I as o}from"./index-Cqe5odbd.js";import"./preload-helper-D7k33qEn.js";import"./agent-logo-DqW8iERJ.js";import"./typograph-BZ_HRgl2.js";const f={title:"Illustrations/IllustrationAdvertising",component:o,parameters:{layout:"centered"}},r={decorators:[t=>e.jsx("div",{style:{width:630,height:630,borderRadius:24,overflow:"hidden",background:"linear-gradient(135deg, #f7f0ff 0%, #ead7fe 30%, #c16ffb 70%, #ff8fda 100%)"},children:e.jsx(t,{})})]};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  decorators: [Story => <div style={{
+    width: 630,
+    height: 630,
+    borderRadius: 24,
+    overflow: 'hidden',
+    background: 'linear-gradient(135deg, #f7f0ff 0%, #ead7fe 30%, #c16ffb 70%, #ff8fda 100%)'
+  }}>
+        <Story />
+      </div>]
+}`,...r.parameters?.docs?.source}}};const l=["Default"];export{r as Default,l as __namedExportsOrder,f as default};
